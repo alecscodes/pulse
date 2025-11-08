bcad07cbe1e4a7d2f39db39670bdaa3394d96e95
alexx <random@no.mail>
2025-11-08 23:01:43 +0100

    chore: remove base configuration from Vite setup
    - Eliminate the base property from vite.config.ts to streamline configuration.
    - This change simplifies the Vite setup for the Laravel application.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/',
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
